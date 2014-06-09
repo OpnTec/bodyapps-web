7db058e788f94a61ed3e5e09f34a3d01126fc175
Vishv Brahmbhatt <vishvbrahmbhatt19@gmail.com>
2014-06-09 20:19:02 +0530

    Just a small change required for testing

--- server.js
+++ server.js
@@ -14,3 +14,5 @@ app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+module.exports = app;

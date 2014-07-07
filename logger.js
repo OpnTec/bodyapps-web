3d3ef5517c4d0a4ebde7fe37b3f909c59497829d
Vishv Brahmbhatt <vishvbrahmbhatt19@gmail.com>
2014-07-07 22:52:42 +0530

    Added a new file for Winston Logger.

---
+++ logger.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2014, Fashiontec (http://fashiontec.org)
+ * Licensed under LGPL, Version 3
+ */
+
+/*
+ * Logger of the bodyapps backend.
+ */
+
+var winston = require('winston');
+var config = require('config');
+
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console) ({level:config.winston.level})
+  ]
+});
+
+module.exports = logger;

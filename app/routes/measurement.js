e1249580fd39caf92bf8deae2bcec63ff781e00a
Vishv Brahmbhatt <vishvbrahmbhatt19@gmail.com>
2014-07-24 17:03:37 +0530

    Making changes for the HDF GET request.

--- app/routes/measurement.js
+++ app/routes/measurement.js
@@ -12,8 +12,13 @@
  */
 
 var Measurement = require('../models/measurement');
+var User = require('../models/user');
+var Image = require('../models/image');
 var validator = require('validator');
 var errorResponse = require('./errorResponse');
+var xml = require('./xmlBuilder');
+var archiver = require('archiver');
+var async = require('async');
 
 function mapMeasurement(req, doc) {
   var data = doc.toJSON();
@@ -42,19 +47,79 @@ module.exports = function(app) {
     })
   });
 
-  app.get('/users/:user_id/measurements/:measurement_id', function(req, res) {
-    var m_id = req.params.measurement_id;
+  app.get('/users/:user_id/measurements/:measurement_id', function(req, res, next) {
+    var measurementId = req.params.measurement_id;
+    var userId = req.params.user_id;
 
-    Measurement.findOne({ m_id: m_id}, function(err, doc) {
-      if(err) return next(err);
-      if(doc) {
-        return res.json(mapMeasurement(req, doc));
-      }
-      return res.status(404).json(errorResponse('Measurement record not found', 404));
-    })
+    if(req.get('Accept') === 'application/json') {
+      Measurement.findOne({ m_id: measurementId}, function(err, doc) {
+        if(err) return next(err);
+        if(doc) {
+          return res.json(mapMeasurement(req, doc));
+        }
+        return res.json(404,
+          errorResponse('Measurement record not found', 404));
+      })
+    }
+    else if (req.get('Accept') === 'application/vnd.valentina.hdf') {
+      var zip = archiver('zip');
+      res.set('Content-type', 'application/vnd.valentina.hdf');
+
+      zip.pipe(res);
+      var fileNameList = [];
+
+      User.findOne({ _id: userId}, function(err, user) {
+        if(err) return next(err);
+
+        if(validator.isNull(user))  return res.send(404,'user not found');
+        Measurement.findOne({ m_id: measurementId}, function(err, measurement) {
+          if(err) return next(err);
+          if(validator.isNull(measurement))  
+            return res.send(404,'measurement not found');
+
+          if(measurement.images.length!=0) {
+            async.each(measurement.images, function(image, callback) {
+              Image.findOne({ _id: image.idref}, function(err, doc) {
+                if(err) return next(err);
+                var fileName = 'pictures/'+ image.idref + '.' + image.type;
+                zip.append(doc.binary_data, {name: fileName});
+                fileNameList.push(fileName);
+                callback();
+              });
+            },
+              function(err) {
+                if(err) next(err);
+                var xmlDoc = xml.returnXML(measurement, user, fileNameList);
+   
+                zip.append(new Buffer(xmlDoc),
+                  { name:'hdf.xml' });
+                zip.finalize(function (err) {
+                  if (err) next(err);
+                  res.end();
+                });
+              }
+            )
+          }
+
+          else {
+            var xmlDoc = xml.returnXML(measurement, user, fileNameList);
+
+            zip.append(new Buffer(xmlDoc),
+              { name:'hdf.xml' });
+            zip.finalize(function (err) {
+              if (err) next(err);
+              res.end();
+            });
+          }
+        });
+      });
+    }
+    else {
+      return res.send(406, 'Not Acceptable Request');
+    }
   });
 
-  app.post('/users/:user_id/measurements', function(req, res) {
+  app.post('/users/:user_id/measurements', function(req, res, next) {
     var body = req.body;
     var personName = body.person.name;
     var personDob = body.person.dob;
@@ -62,7 +127,8 @@ module.exports = function(app) {
 
     if(validator.isNull(personName) || validator.isNull(personDob) 
       || validator.isNull(personGender)) {
-        return res.status(400).json(errorResponse('Email not found', 400));
+        return res.json(400,
+          errorResponse('person name, date of birth or gender is missing', 400));
     }
     Measurement.create(body, function(err, doc) {
       if(err) return next(err);

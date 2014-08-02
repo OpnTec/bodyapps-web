9598a31b81020c142bc1620928421467fef78db6
Vishv Brahmbhatt <vishvbrahmbhatt19@gmail.com>
2014-08-02 20:05:43 +0530

    Adding a global function which sends email via nodemailer.

---
+++ app/lib/sendmail.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2014, Fashiontec (http://fashiontec.org)
+ * Licensed under LGPL, Version 3
+*/
+
+/*
+ * Handles mail transport of nodemailer.
+ * This function sends email via nodemailer.
+ */
+
+var nodemailer = require('nodemailer');
+var config = require('config');
+
+var transport = nodemailer.createTransport(config.transport.type,
+  config.transportOptions);
+
+module.exports.sendmail = function(message, callback) {
+  transport.sendMail(message, function(err, res) {
+    if(err) callback(err);
+    transport.close();
+    callback();
+  });
+}
